3adf54f6948e26ce63653a3da3b0083fd798e25d
Youri Ackx <1193689+yackx@users.noreply.github.com>
2023-03-27 17:18:42 +0200

    Improve magic encoding

--- src/escp/commands/magic_encoding.py
+++ src/escp/commands/magic_encoding.py
@@ -4,6 +4,17 @@ from .parameters import CharacterSetVariant
 # International character set [R-41]
 # Very basic table, far from complete
 magic_encoding = {
+    "€": (CharacterSetVariant.USA, b'\xee'),  # looks similar ∊
+    "☺︎": (CharacterSetVariant.USA, b'\x01'),
+    "☺": (CharacterSetVariant.USA, b'\x01'),
+    "☻": (CharacterSetVariant.USA, b'\x02'),
+    "♥︎": (CharacterSetVariant.USA, b'\x03'),
+    "♡": (CharacterSetVariant.USA, b'\x03'),
+    "♦︎": (CharacterSetVariant.USA, b'\x04'),
+    "♢": (CharacterSetVariant.USA, b'\x04'),
+    "♣︎": (CharacterSetVariant.USA, b'\x05'),
+    "♧": (CharacterSetVariant.USA, b'\x05'),
+    "§": (CharacterSetVariant.USA, b'\x15'),
     "#": (CharacterSetVariant.USA, b'\x23'),
     "$": (CharacterSetVariant.USA, b'\x24'),
     "@": (CharacterSetVariant.USA, b'\x40'),
@@ -25,5 +36,5 @@ magic_encoding = {
     "è": (CharacterSetVariant.FRANCE, b'\x7d'),
     "¨": (CharacterSetVariant.FRANCE, b'\x7e'),
     "©": (CharacterSetVariant.LEGAL, b'\x7b'),
-    "®": (CharacterSetVariant.LEGAL, b'\x7c'),
+    "®": (CharacterSetVariant.LEGAL, b'\x7c')
 }
